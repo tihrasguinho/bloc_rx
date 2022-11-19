77c6e3cf4b82e0bf75a0930cdc8b50b419e9c601
Tiago Alves <tiago.pessoal93@gmail.com>
2022-11-19 19:20:57 -0300

    mais ajustes

--- lib/src/bloc_rx.dart
+++ lib/src/bloc_rx.dart
@@ -7,7 +7,7 @@ import 'package:rxdart/rxdart.dart';
 
 /// [E] Events and [S] States
 abstract class BlocRX<E extends Object?, S extends Object?> {
-  late BehaviorSubject<E> _controller;
+  final BehaviorSubject<E> _controller = BehaviorSubject<E>();
 
   late S _state;
 
@@ -15,12 +15,10 @@ abstract class BlocRX<E extends Object?, S extends Object?> {
   S get state => _state;
 
   /// Stream that provides the output of BLoC!
-  late Stream<S> stream;
+  Stream<S> get stream => _controller.stream.switchMap<S>(mapEventsToState);
 
   BlocRX(S initialState) {
     _state = initialState;
-    _controller = BehaviorSubject<E>();
-    stream = _controller.stream.switchMap<S>(mapEventsToState);
 
     // Listen to new states and store it
     stream.listen((newState) => _state = newState);
